36dd101dbe3da1b10af524e8933b8b3a6a4cb22b
Joshua <jballard@unca.edu>
2019-10-23 18:59:12 -0400

    updates to the look of the screens still need to change the button shapes
    the buttons work as well as making it fit a tablet and locking the screen size

--- app/src/main/java/com/b/earthdrone/Live_Activity.java
+++ app/src/main/java/com/b/earthdrone/Live_Activity.java
@@ -1,4 +1,44 @@
 package com.b.earthdrone;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Live_Activity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Live_Activity extends AppCompatActivity {
+    private Button mMap_button;
+    private Button mControl_button;
+    private Button mDash_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.live_view);
+        mControl_button = (Button) findViewById(R.id.Control_button);
+        mControl_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.control_view);
+            }
+        });
+
+        mMap_button = (Button) findViewById(R.id.Live_button);
+        mMap_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.map_view);
+            }
+        });
+
+
+        mDash_button = (Button) findViewById(R.id.Live_button);
+        mDash_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.dash_view);
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/b/earthdrone/Map_Activity.java
+++ app/src/main/java/com/b/earthdrone/Map_Activity.java
@@ -3,12 +3,43 @@ package com.b.earthdrone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Map_Activity extends AppCompatActivity {
+
+private Button mLive_button;
+private Button mDash_button;
+private Button mControl_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.live_view);
+        setContentView(R.layout.map_view);
+        mLive_button = (Button) findViewById(R.id.Live_button);
+        mLive_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.live_view);
+            }
+        });
+
+        mControl_button = (Button) findViewById(R.id.Control_button);
+        mControl_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.control_view);
+            }
+        });
+
+        mDash_button = (Button) findViewById(R.id.Dash_button);
+        mDash_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.dash_view);
+            }
+        });
     }
+
+
 }
